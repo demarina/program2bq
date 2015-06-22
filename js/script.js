16a0e399141a3564088a81d181b9028f3aa5c0a0
System Administrator <root@PepeBotella.local>
2015-06-22 22:24:08 +0200

    Part AJAX to send mail done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,48 @@
 $(document).ready(function(){
 	$("input").focus(function(){
-		if($(this).attr("type") == "text")
-			$(this).parent().parent().addClass("active");
-			}).blur(function(){
-				if($(this).val() == "")
-					$(this).parent().parent().removeClass("active");
-			})
+		$(this).parent().parent().addClass("active");
+		}).blur(function(){
+			if($(this).val() == "")
+				$(this).parent().parent().removeClass("active");
+		})
 
+	$("#entrar").click(function(){
+		var name = $("#name").val();
+		var mail = $("#mail").val();
+		var ident = $("#ident").val();
+
+		if((name == "") || (mail == "") || (ident == ""))
+			alert("Rellene todos los campos por favor");
+		else{
+			$.ajax({
+
+				beforeSend: function(){
+					console.log("ENTRO");
+				},
+
+				url: "php/mail.php",
+				type: "POST",
+
+				data: {
+					"name": name,
+					"mail": mail,
+					"ident": ident,
+				},
+
+			    success: function(data) {
+
+
+			    },
+
+			    error: function(jqXHR,estado,error) {
+			    	console.log(estado);
+			    	console.log(error);
+			    },
+
+			    complete: function(jqXHR,estado) {
+			    	console.log(estado);
+			    },
+			});
+		}
+	});
 });
